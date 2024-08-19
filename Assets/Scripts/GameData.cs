8ffd8369da16eadcae7be489d93224bd6f1cace5
Maksim Kryvanos <maksim.kryvanosint@unity3d.com>
2024-08-19 23:06:20 +0200

    UI_Update

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -1,5 +1,9 @@
 public static class GameData
 {
-    public static int GuessedNumber { get; set; }
+    #region Properties
+
     public static int GuessCount { get; set; }
+    public static int GuessedNumber { get; set; }
+
+    #endregion
 }
